b0542d3600e658645d14590cfc98f55ca645c011
Alison Ferreira <95418705+Alison695@users.noreply.github.com>
2025-03-19 18:37:57 -0300

    Criar Primeira Atividade 10 - 20/Q10.java

---
+++ Primeira Atividade 10 - 20/Q10.java
@@ -0,0 +1,23 @@
+import javax.swing.JOptionPane;
+
+public class ValoresNegativosJOptionPane {
+    public static void main(String[] args) {
+        double[] numeros = new double[5];
+        int contadorNegativos = 0;
+
+        for (int i = 0; i < 5; i++) {
+            String input = JOptionPane.showInputDialog("Digite o " + (i + 1) + "º valor:");
+            try {
+                numeros[i] = Double.parseDouble(input);
+                if (numeros[i] < 0) {
+                    contadorNegativos++;
+                }
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Entrada inválida. Digite um número.");
+                i--;
+            }
+        }
+
+        JOptionPane.showMessageDialog(null, "Quantidade de valores negativos: " + contadorNegativos);
+    }
+}
