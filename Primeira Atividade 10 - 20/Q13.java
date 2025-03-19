b35613ccf565e3a1d883b81687a3ef9840b4a550
Alison Ferreira <95418705+Alison695@users.noreply.github.com>
2025-03-19 18:50:28 -0300

    Criar Q13.java

---
+++ Primeira Atividade 10 - 20/Q13.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class Q13 {
+    public static void main(String[] args) {
+        String resultado = "Números que divididos por 11 dão resto 5 no intervalo de 1000 a 1999:\n";
+
+        for (int numero = 1000; numero <= 1999; numero++) {
+            if (numero % 11 == 5) {
+                resultado += numero + "\n";
+            }
+        }
+
+        JOptionPane.showMessageDialog(null, resultado);
+    }
+}
